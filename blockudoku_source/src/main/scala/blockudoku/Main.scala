2de404330918c305fe2400f6565c4375e71617c6
Tom Berger <berger.tom@mail.de>
2024-10-16 15:06:08 +0200

    feat: add "tom war hier"

--- blockudoku_source/src/main/scala/blockudoku/Main.scala
+++ blockudoku_source/src/main/scala/blockudoku/Main.scala
@@ -2,9 +2,7 @@ package blockudoku
 
 import blockudoku.windows.{ConsoleWindow, Window}
 
-import sys.process._
-
-
 @main def main(): Unit =
   val window: Window = ConsoleWindow()
   window.display()
+  println("Tom war hier")
