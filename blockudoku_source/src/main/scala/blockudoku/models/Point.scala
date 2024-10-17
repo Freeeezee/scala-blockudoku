f061e0f5a920b95799eab30994780492501d2dee
Tom Berger <berger.tom@mail.de>
2024-10-17 10:52:24 +0200

    feat: make points addable

--- blockudoku_source/src/main/scala/blockudoku/models/Point.scala
+++ blockudoku_source/src/main/scala/blockudoku/models/Point.scala
@@ -1,3 +1,8 @@
 package blockudoku.models
 
-case class Point(xPos : Int, yPos : Int)
+import scala.annotation.targetName
+
+case class Point(xPos : Int, yPos : Int){
+  @targetName("add")
+  def +(that: Point): Point = Point(this.xPos + that.xPos, this.yPos + that.yPos)
+}
