479e7d695d1e21f54a264406aa17390e63e93c59
Tom Berger <berger.tom@mail.de>
2024-11-07 20:15:36 +0100

    test: add test for selecting an element

--- blockudoku_source/src/test/scala/test/views/ConsoleElementViewSpec.scala
+++ blockudoku_source/src/test/scala/test/views/ConsoleElementViewSpec.scala
@@ -3,6 +3,7 @@ package test.views
 import test.{RandomMock, UnitSpec}
 import blockudoku.controllers.{ElementController, GridController}
 import blockudoku.views.console.ConsoleElementView
+import blockudoku.views.console.composed.ComposedConsoleFormatter
 import blockudoku.windows.FocusManager
 import blockudoku.windows.FocusState.Elements
 
@@ -16,6 +17,17 @@ class ConsoleElementViewSpec extends UnitSpec {
                                          |
                                          |XX               XX               XX
                                          |""".stripMargin.replace("\r\n", "\n"))
+    }
+
+    "select the highlighted element in ElementController" in {
+      val elementController = ElementController(RandomMock(), new FocusManager(Elements))
+      val gridController = GridController(9, 9)
+      val elementView = ConsoleElementView(gridController, elementController, new FocusManager(Elements))
+      val formatter = ComposedConsoleFormatter.create(elementView.consoleElement)
+
+      formatter.select()
+
+      elementController.selectedElement should be (Some(elementController.elements.head))
     }
   }
 }
