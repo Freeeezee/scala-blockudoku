3c74f7395f07372d369b8f47c1bc48351900acef
Tom Berger <berger.tom@mail.de>
2024-12-12 14:03:29 +0100

    feat: make thread cancelable

--- src/main/scala/blockudoku/services/ApplicationThread.scala
+++ src/main/scala/blockudoku/services/ApplicationThread.scala
@@ -1,11 +1,46 @@
 package blockudoku.services
 
-import java.util.concurrent.Executors
-import scala.concurrent.{ExecutionContext, Future}
+import java.util.concurrent.{CancellationException, Executors}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
 class ApplicationThread {
-  protected implicit val context: ExecutionContext =
-    ExecutionContext.fromExecutorService(Executors.newSingleThreadExecutor())
+  private val executor = Executors.newSingleThreadExecutor()
+  protected implicit val context: ExecutionContext = ExecutionContext.fromExecutorService(executor)
 
-  def run(code: => Unit): Unit = Future(code)
+  def run(code: => Unit): CancelableTask = {
+    val promise = Promise[Unit]()
+
+    val future = Future {
+      try {
+        code
+        promise.success(())
+      } catch {
+        case e: CancellationException =>
+          promise.failure(e)
+        case t: Throwable =>
+          promise.failure(t)
+      }
+    }
+
+    new CancelableTask(future, promise, executor)
+  }
+}
+
+class CancelableTask(
+                      val future: Future[Promise[Unit]],
+                      private val promise: Promise[Unit],
+                      private val executor: java.util.concurrent.ExecutorService
+                    ) {
+  def cancel(): Boolean = {
+    // Attempt to cancel the running task
+    val cancelResult = executor.shutdownNow()
+    promise.tryFailure(new CancellationException("Task was cancelled"))
+
+    // Return true if the task was successfully cancelled
+    !cancelResult.isEmpty
+  }
+
+  def isCancelled: Boolean = promise.future.value.exists(_.isFailure)
+
+  def isCompleted: Boolean = future.isCompleted
 }
