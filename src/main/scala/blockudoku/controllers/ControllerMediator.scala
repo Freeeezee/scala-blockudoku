cfc90a3edf4a55a48243b4520cbc279e06a65ab1
Tom Berger <berger.tom@mail.de>
2024-12-20 08:35:58 +0100

    feat: add trait for controller mediator

--- src/main/scala/blockudoku/controllers/ControllerMediator.scala
+++ src/main/scala/blockudoku/controllers/ControllerMediator.scala
@@ -2,27 +2,8 @@ package blockudoku.controllers
 
 import blockudoku.commands.Snapshotable
 import blockudoku.models.Element
-import blockudoku.windows.{FocusManager, FocusState}
 
-class ControllerMediator(gridController: GridController, elementController: ElementController, focusManager: FocusManager) extends Snapshotable[?] {
-  def setElement(element: Element, pos: Int) : Unit = {
-    if !gridController.setElement(element, pos) then return
-    elementController.regenerate(element.slot)
-    elementController.resetSelectedElement()
-    focusManager.setFocusState(FocusState.Elements)
-  }
-  def selectElement(element: Element): Unit = {
-    elementController.selectElement(element)
-    focusManager.setFocusState(FocusState.Grid)
-  }
-  def createSnapshot(): Unit = {
-    gridController.createSnapshot()
-    elementController.createSnapshot()
-    focusManager.createSnapshot()
-  }
-  def revertSnapshot(): Unit = {
-    gridController.revertSnapshot()
-    elementController.revertSnapshot()
-    focusManager.revertSnapshot()
-  }
+trait ControllerMediator extends Snapshotable[?] {
+  def setElement(element: Element, pos: Int) : Unit
+  def selectElement(element: Element): Unit
 }
---
+++ src/main/scala/blockudoku/controllers/ControllerMediatorImpl.scala
@@ -0,0 +1,28 @@
+package blockudoku.controllers
+
+import blockudoku.commands.Snapshotable
+import blockudoku.models.Element
+import blockudoku.windows.{FocusManager, FocusState}
+
+class ControllerMediatorImpl(gridController: GridController, elementController: ElementController, focusManager: FocusManager) extends ControllerMediator, Snapshotable[?] {
+  override def setElement(element: Element, pos: Int) : Unit = {
+    if !gridController.setElement(element, pos) then return
+    elementController.regenerate(element.slot)
+    elementController.resetSelectedElement()
+    focusManager.setFocusState(FocusState.Elements)
+  }
+  override def selectElement(element: Element): Unit = {
+    elementController.selectElement(element)
+    focusManager.setFocusState(FocusState.Grid)
+  }
+  def createSnapshot(): Unit = {
+    gridController.createSnapshot()
+    elementController.createSnapshot()
+    focusManager.createSnapshot()
+  }
+  def revertSnapshot(): Unit = {
+    gridController.revertSnapshot()
+    elementController.revertSnapshot()
+    focusManager.revertSnapshot()
+  }
+}
